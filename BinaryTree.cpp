eac168ce86c7222224cc6c644e55c003ab75bf09
OnlyblackTea <804559276@qq.com>
2019-10-15 08:55:26 +0800

    finished

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -6,16 +6,13 @@
 using namespace std;
 int n,m,btree[500010],a[500010];
 int lowbit(int x){return x&(-x);}
-void add(int x,int k){
-    for(int i=x;i<n;i+=lowbit(i)){
-        
-    }
-}
+void add(int x,int k){ for(int i=x;i<n;i+=lowbit(i)) {btree[i]+=k;} }
+int getsum(int x){ int ans=0; for(int i=x;i;i-=lowbit(i))ans+=c[i]; return ans; }
 
 int main(){
     scanf("%d%d",&n,&m);
-    for(int i=1;i<=n;i++){
-        scanf("%d",&a[i]);
-        add(i,a[i]);
-    }
+    for(int i=1;i<=n;i++){ scanf("%d",&a[i]); add(i,a[i]); }
+    int x,y,order;
+    for(int i=1;i<=m;i++){ scanf("%d%d%d",&order,&x,&y); if (order==1) { add(x,y);a[x]+=y; } else if (order==2) { printf("%d\n",getsum(y)-getsum(x-1)); } }
+    return 0;
 }

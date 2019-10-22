6d40cca9739baa403e7f582b60f5d5bce4fe66b8
OnlyblackTea <804559276@qq.com>
2019-10-22 21:05:32 +0800

    update 10.22

--- gbnxd.cpp
+++ gbnxd.cpp
@@ -4,10 +4,10 @@
 #include<algorithm>
 #include<iostream>
 using namespace std;
-int a[40001];
-int t[40001];
+int a[500001];
+int t[500001];
 
-void merge(int l,int r,int m,int *a,int *t,int &ans){
+void merge(int l,int r,int m,int *a,int *t,long long &ans){
     int i=l;
     int j=m+1;
     int k=l;
@@ -26,7 +26,7 @@ void merge(int l,int r,int m,int *a,int *t,int &ans){
         a[i]=t[i];
     }
 }
-void merge_sort(int l,int r,int *a,int *t,int &ans){
+void merge_sort(int l,int r,int *a,int *t,long long &ans){
     if(l<r){
         int mid=(l+r)>>1;
         merge_sort(l,mid,a,t,ans);
@@ -43,6 +43,6 @@ int main(){
     }
     int ans=0;
     merge_sort(1,n,a,t,ans);
-    printf("%d\n",ans);
+    printf("%lld\n",ans);
     return 0;
 }

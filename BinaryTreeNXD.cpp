1af28324cd38c8d26125c1f6a0e1754f9f27ef54
OnlyblackTea <804559276@qq.com>
2019-10-22 20:55:51 +0800

    update 10.22

--- BinaryTreeNXD.cpp
+++ BinaryTreeNXD.cpp
@@ -4,27 +4,37 @@
 #include<cmath>
 #include<algorithm>
 using namespace std;
-
 inline void read(int &n){
     register char c='*';
     for(n=0;!isdigit(c);c=getchar());
     for(;isdigit(c);n=n*10+c-'0',c=getchar());
 }
 
-int C[],n;
-int lowbit(int x){return x&(-x);}
-void add(int x,int k){
-    for(int i=x;i<=n;i+=lowbit(i)){
-        C[i]+=k;
-    }
+struct A{
+    int id,v;
+}a[500010];
+bool cmp(A a, A b){
+    if(a.v==b.v){return a.id>b.id;}
+    else{return a.v>b.v;}
 }
-int getsum(int x){
-    int ans=0;
-    for(int i=x;i>0;i-=lowbit(i)){
-        ans+=C[i];
-    }
-    return ans;
-}
-int main(){
+int C[500010],n;
 
+int lowbit(int x){return x&(-x);}
+void add(int x,int k){for(int i=x;i<=n;i+=lowbit(i)){C[i]+=k;}}
+int getsum(int x){int ans=0;for(int i=x;i>0;i-=lowbit(i)){ans+=C[i];}return ans;}
+
+int main(){
+    long long ans=0;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        scanf("%d",&a[i].v);
+        a[i].id=i;
+    }
+    sort(a+1,a+n+1,cmp);
+    for(int i=1;i<=n;i++){
+        ans+=getsum(a[i].id);
+        add(a[i].id,1);
+    }
+    printf("%lld\n",ans);
+    return 0;
 }
---
+++ gbnxd.cpp
@@ -0,0 +1,48 @@
+#include<cstdio>
+#include<cmath>
+#include<cstring>
+#include<algorithm>
+#include<iostream>
+using namespace std;
+int a[40001];
+int t[40001];
+
+void merge(int l,int r,int m,int *a,int *t,int &ans){
+    int i=l;
+    int j=m+1;
+    int k=l;
+    while(i<=m&&j<=r){
+        if(a[i]>a[j]){
+            t[k++]=a[j++];
+            ans+=m-i+1;
+        }
+        else{
+            t[k++]=a[i++];
+        }
+    }
+    while(i<=m) t[k++]=a[i++];
+    while(j<=r) t[k++]=a[j++];
+    for(i=l;i<=r;i++){
+        a[i]=t[i];
+    }
+}
+void merge_sort(int l,int r,int *a,int *t,int &ans){
+    if(l<r){
+        int mid=(l+r)>>1;
+        merge_sort(l,mid,a,t,ans);
+        merge_sort(mid+1,r,a,t,ans);
+        merge(l,r,mid,a,t,ans);
+    }
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    int i;
+    for(i=1;i<=n;i++){
+        scanf("%d",a+i);
+    }
+    int ans=0;
+    merge_sort(1,n,a,t,ans);
+    printf("%d\n",ans);
+    return 0;
+}
